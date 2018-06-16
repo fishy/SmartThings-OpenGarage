f516e0f4d3a8108bf35e4b354a0c8cf526f1d392
Yuxuan 'fishy' Wang <wang@yuxuan.org>
2018-06-16 14:19:28 -0700

    Add open-garage-direct device handler

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # SmartThings and OpenGarage
 
-This repository provides a [SmartThings](https://www.smartthings.com/)
-Device Handler for [OpenGarage](https://opengarage.io),
+This repository provides [SmartThings](https://www.smartthings.com/)
+Device Handlers for [OpenGarage](https://opengarage.io),
 and two SmartApps to help you automate your garage door.
 
 ## Device Handler (via Blynk)
@@ -40,6 +40,77 @@ You could setup your own proxy using blynk-proxy,
 or if after understanding the
 [risks](https://github.com/fishy/blynk-proxy/blob/master/README.md#should-i-use-your-heroku-app)
 you still don't mind, you could use `https://blynk-proxy.herokuapp.com`.
+
+### State Refresh Rate
+
+We couldn't push the garage door state to SmartThings right now so the update of
+the state rely on refresh requests initiated from SmartThings to Blynk server.
+This preference controls how frequent we send those refresh requests.
+
+If you don't have a Contact Sensor setup with your OpenGarage,
+you would need a more frequent refresh rate to keep the state up-to-date.
+
+## Device Handler (Direct)
+
+[open-garage-direct.groovy](open-garage-direct.groovy)
+
+First you need to setup your [OpenGarage](https://opengarage.io) normally,
+the [Blynk](https://blynk.cc) integration steps are optional (not needed for
+this integration).
+
+Then you need to make your local OpenGarage web UI available to the internet.
+This usually can be done in two ways:
+
+1. On your router, map your OpenGarage's IP and 80 port to an external port.
+   Please note that this way the communication is not encrypted, your garage
+	 controller web UI is exposed to the internet without protection,
+	 so it's not recommended.
+
+2. In your home, run a 24/7 machine with a reverse proxy (e.g.
+   [nginx](https://www.nginx.com/)). You can configure HTTPS and/or HTTP
+   authentication this way, but you need a 24/7 machine to make it work.
+
+After you exposed your OpenGarage web UI to the internet, login to your
+[SmartThings developer account](https://graph.api.smartthings.com/),
+create a new Device Handler, and import the code.
+
+After that, you can create a new Device using the Device Handler you just
+created.
+On your SmartThings app, choose "Add a Thing" and the device you just created
+from developer IDE will appear. Confirm it.
+
+There are few required preferences you must set before you can really use the
+device. You can set them up on either the app or IDE:
+
+### Device key
+
+This is the password you used on OpenGarage web UI to do actions and change
+options. The factory default is `opendoor`.
+
+### HTTP(s) prefix
+
+This is the way you exposed the web UI to the internet.
+If you just mapped the port on your router,
+this should be something like `http://yourip:port`.
+If you setup a reverse proxy with HTTPS,
+this should be something like `https://yourdomain`.
+
+### HTTP Authentication header
+
+This is only needed if you setup a reverse proxy and setup HTTP authentication.
+If you just use simple username/password authentication,
+it should be something like `Basic dXNlcm5hbWU6cGFzc3dvcmQ=`,
+while the part after `Basic ` is the base64 encoded username and password.
+You can get that in Python easily:
+
+```python
+>>> from base64 import b64encode
+>>> b64encode('username:password')
+'dXNlcm5hbWU6cGFzc3dvcmQ='
+```
+
+If you setup other HTTP authentication methods,
+e.g. Digest, refer to RFC to see how to get the `Authentication` HTTP header.
 
 ### State Refresh Rate
 
---
+++ open-garage-direct.groovy
@@ -0,0 +1,471 @@
+/**
+ *  SmartThings device handler for OpenGarage.
+ *
+ *  Copyright 2018 Yuxuan "fishy" Wang
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ *
+ *
+ *  Some of the code are from https://github.com/adamheinmiller/ST_MyQ by Adam Heinmiller,
+ *  used under Apache License.
+ *
+ */
+
+metadata {
+	definition(name: "OpenGarage-Direct", namespace: "fishy", author: "Yuxuan Wang") {
+		capability "Contact Sensor"
+		capability "Door Control"
+		capability "Garage Door Control"
+		capability "Momentary"
+		capability "Polling"
+		capability "Refresh"
+		capability "Switch"
+
+		attribute "doorStatus", "string"
+		attribute "lastHttpStatus", "string"
+	}
+
+	preferences {
+		input(
+			name: "device_key",
+			type: "text",
+			title: "Device key",
+			description: "Factory default is \"opendoor\"",
+			required: true,
+		)
+
+		input(
+			name: "http_prefix",
+			type: "text",
+			title: "HTTP(s) prefix",
+			description: "Example: \"http://yourip:8080\" or \"https://opengarage.myhome.com\"",
+			required: true,
+		)
+
+		input(
+			name: "http_auth",
+			type: "text",
+			title: "HTTP Authorization header",
+			description: "Optional. Example: \"Basic dXNlcm5hbWU6cGFzc3dvcmQ=\"",
+			required: false,
+		)
+
+		input(
+			name: "refresh_rate",
+			type: "enum",
+			title: "State Refresh Rate",
+			options: [
+				"Every minute",
+				"Every 5 minutes",
+				"Every 10 minutes",
+				"Every 15 minutes",
+				"Every 30 minutes",
+				"Every hour",
+				"Disabled",
+			],
+			description: "Only disable it if you have another contact sensor hooked on the garage door",
+			required: true,
+		)
+	}
+
+	tiles {
+		standardTile("sDoorToggle", "device.doorStatus", width: 1, height: 1, canChangeIcon: false) {
+			state(
+				"default",
+				label: "",
+			)
+
+			state(
+				"unknown",
+				label: "Unknown",
+				icon: "st.unknown.unknown.unknown",
+				action: "refresh.refresh",
+				backgroundColor: "#afafaf",
+			)
+			state(
+				"door_not_found",
+				label: "Not Found",
+				backgroundColor: "#CC1821",
+			)
+
+			state(
+				"stopped",
+				label: "Stopped",
+				icon: "st.contact.contact.open",
+				action: "close",
+				backgroundColor: "#ffdd00",
+			)
+			state(
+				"closed",
+				label: "Closed",
+				icon: "st.doors.garage.garage-closed",
+				action: "open",
+				backgroundColor: "#79b820",
+			)
+			state(
+				"closing",
+				label: "Closing",
+				icon: "st.doors.garage.garage-closing",
+				backgroundColor: "#ffdd00",
+			)
+			state(
+				"open",
+				label: "Open",
+				icon: "st.doors.garage.garage-open",
+				action: "close",
+				backgroundColor: "#ffdd00",
+			)
+			state(
+				"opening",
+				label: "Opening",
+				icon: "st.doors.garage.garage-opening",
+				backgroundColor: "#ffdd00",
+			)
+			state(
+				"moving",
+				label: "Moving",
+				icon: "st.motion.motion.active",
+				action: "refresh.refresh",
+				backgroundColor: "#ffdd00",
+			)
+		}
+
+		standardTile("sRefresh", "device.doorStatus", inactiveLabel: false, decoration: "flat") {
+			state(
+				"default",
+				label: "",
+				action: "refresh.refresh",
+				icon: "st.secondary.refresh",
+			)
+		}
+
+		standardTile("sContact", "device.contact") {
+			state(
+				"open",
+				label: "${name}",
+				icon: "st.contact.contact.open",
+				backgroundColor: "#ffa81e",
+			)
+			state(
+				"closed",
+				label: "${name}",
+				icon: "st.contact.contact.closed",
+				backgroundColor: "#79b821",
+			)
+		}
+
+		main(["sDoorToggle"])
+		details(["sDoorToggle", "sRefresh"])
+	}
+}
+
+
+def installed() {
+	log.debug "Installing OpenGarage Door"
+
+	checkRefresh()
+	refresh()
+}
+
+def updated() {
+	log.debug "Updating OpenGarage Door"
+
+	checkRefresh()
+	refresh()
+}
+
+def parse(description) {
+	log.debug "Parsing '${description}'"
+	// TODO: handle 'contact' attribute
+	// TODO: handle 'switch' attribute
+}
+
+def push() {
+	log.debug "Executing 'push'"
+
+	def initStatus
+	getDoorStatus() { status -> initStatus = status }
+
+	def target
+	if (initStatus == "closed" || initStatus == "closing" || initStatus == "stopped" || initStatus == "moving") {
+		log.debug "Door is in a closed status, opening"
+		target = "open"
+	} else if (initStatus == "open" || initStatus == "opening") {
+		log.debug "Door is in an open status, closing"
+		target = "closed"
+	} else if (initStatus == "unknown") {
+		log.debug "Door is in an unknown state, doing nothing"
+		return
+	}
+
+	flipDoor()
+	refreshUntil(target)
+}
+
+def poll() {
+	log.debug "OpenGarage Polling"
+	refresh()
+}
+
+def refresh() {
+	doRefresh()
+}
+
+def checkRefresh() {
+	switch (refresh_rate.toLowerCase()) {
+	case "disabled":
+		unschedule(doRefresh)
+		doRefresh()
+		break
+	case "every 5 minutes":
+		runEvery5Minutes(doRefresh)
+		break
+	case "every 10 minutes":
+		runEvery10Minutes(doRefresh)
+		break
+	case "every 15 minutes":
+		runEvery15Minutes(doRefresh)
+		break
+	case "every 30 minutes":
+		runEvery30Minutes(doRefresh)
+		break
+	case "every hour":
+		runEvery1Hour(doRefresh)
+		break
+	case "every minute":
+	default:
+		runEvery1Minute(doRefresh)
+		break
+	}
+}
+
+def doRefresh() {
+	log.debug "Refreshing Door State"
+
+	getDoorStatus() { status ->
+		setDoorState(status)
+		log.debug "Door Status: $status"
+	}
+}
+
+def afterForceRefresh(status, startTime) {
+	def time = (now() - startTime) / 1000
+	log.debug "Final Door Status: $status, took $time seconds"
+	setDoorState(status)
+}
+
+def forceRefreshUntil(data) {
+	def timestamp = now()
+	def target = data.targetStatus
+	log.debug "forceRefreshUntil: ${new Date()}, timestamp: $timestamp, stops at ${data.stopAt}, target status: $target"
+	def scheduleNext = true
+	if (timestamp >= data.stopAt) {
+		log.debug "Stopping refreshing..."
+		getDoorStatus() { status ->
+			afterForceRefresh(status, data.startTime)
+		}
+		scheduleNext = false
+		return
+	}
+	getDoorStatus() { status ->
+		log.debug "forceRefreshUntil: get door status: $status"
+		if (status == target) {
+			log.debug "Got target status $status, stopping refreshing..."
+			afterForceRefresh(status, data.startTime)
+			scheduleNext = false
+			return
+		}
+	}
+	if (scheduleNext) {
+		def options = [
+			overwrite: true,
+			data: data,
+		]
+		runIn(3, forceRefreshUntil, options)
+	} else {
+		unschedule(forceRefreshUntil)
+	}
+}
+
+def refreshUntil(target) {
+	log.debug "refreshUntil: $target"
+	def maxMin = 5
+	def timestamp = now() + 60 * 1000 * maxMin
+	def data = [
+		startTime: now(),
+		stopAt: timestamp,
+		targetStatus: target,
+	]
+	forceRefreshUntil(data)
+}
+
+def on() {
+	log.debug "Executing 'on'"
+
+	open()
+}
+
+def off() {
+	log.debug "Executing 'off'"
+
+	close()
+}
+
+def open() {
+	log.debug "Opening Door"
+
+	openDoor()
+	setDoorState("opening")
+	refreshUntil("open")
+}
+
+def close() {
+	log.debug "Closing Door"
+
+	closeDoor()
+	setDoorState("closing")
+	refreshUntil("closed")
+}
+
+def getDoorStatus(callback = {}) {
+	callApiGet("/jc") { resp ->
+		def value = resp.data.door
+		def state
+		if (value != 0 && value != 1) {
+			// Not json parsed, parse manually
+			// str should be something like:
+			// {"dist":146,"door":0,"vehicle":1,...}
+			def str = resp.data as String
+			def suffix = str.split('"door":')[1]
+			value = suffix.split(",")[0] as long
+		}
+		if (value == 0) {
+			state = "closed"
+		} else if (value == 1) {
+			state = "open"
+		}
+		log.debug "getDoorStatus: value = $value, state = $state"
+		callback(state)
+	}
+}
+
+def openDoor() {
+	def body = [
+		dkey: device_key,
+		open: 1,
+	]
+	callApiGet("/cc", body) { resp ->
+		// if error, do something?
+	}
+}
+
+def closeDoor() {
+	def body = [
+		dkey: device_key,
+		close: 1,
+	]
+	callApiGet("/cc", body) { resp ->
+		// if error, do something?
+	}
+}
+
+def flipDoor() {
+	def body = [
+		dkey: device_key,
+		click: 1,
+	]
+	callApiGet("/cc", body) { resp ->
+		// if error, do something?
+	}
+}
+
+def setContactSensorState(status) {
+	// Sync contact sensor
+	if (status == "open" || status == "opening" || status == "stopped") {
+		sendEvent(
+			name: "contact",
+			value: "open",
+			displayed: true,
+			descriptionText: "Contact is open",
+		)
+		sendEvent(
+			name: "switch",
+			value: "on",
+			displayed: true,
+			descriptionText: "Switch is on",
+		)
+	} else if (status == "closed" || status == "closing") {
+		sendEvent(
+			name: "contact",
+			value: "closed",
+			displayed: true,
+			descriptionText: "Contact is closed",
+		)
+		sendEvent(
+			name: "switch",
+			value: "off",
+			displayed: true,
+			descriptionText: "Switch is off",
+		)
+	}
+}
+
+def setDoorState(status) {
+	sendEvent(
+		name: "doorStatus",
+		value: status,
+		displayed: true,
+		descriptionText: "Door is $status",
+	)
+	setContactSensorState(status)
+}
+
+def callApiGet(apipath, callback) {
+	return callApiGet(apipath, null, callback)
+}
+
+def callApiGet(apipath, query, callback) {
+	def headers = []
+	if (http_auth != "") {
+		headers = [
+			Authorization: http_auth,
+		]
+	}
+	def params = [
+		uri: http_prefix,
+		path: apipath,
+		query: query,
+		headers: headers,
+	]
+
+	log.debug "http get params: $params"
+
+	try {
+		httpGet(params) { resp ->
+			log.debug "response data: ${resp.data}"
+			sendEvent(
+				name: "lastHttpStatus",
+				value: "succeed",
+				displayed: true,
+				descriptionText: "HTTP request succeeded",
+			)
+			callback(resp)
+		}
+	} catch (e) {
+		log.debug "API Error: $e"
+		setDoorState("unknown")
+		sendEvent(
+			name: "lastHttpStatus",
+			value: "failed",
+			displayed: true,
+			descriptionText: "HTTP request failed",
+		)
+	}
+}
