9271bd066ae63accad29d3650d5916cc50ab994d
Yuxuan 'fishy' Wang <wang@yuxuan.org>
2018-11-22 13:46:53 -0800

    Handle refresh exception

--- garage-door-helper.groovy
+++ garage-door-helper.groovy
@@ -111,7 +111,17 @@ def getMaxRefresh() {
 def forceRefreshGarageState(data) {
 	def timestamp = now()
 	log.debug "forceRefreshGarageState: ${new Date()}, timestamp: $timestamp, stops at ${data.stopAt}"
-	door.refresh()
+	try {
+		door.refresh()
+	} catch (Exception e) {
+		// Sometimes SmartThings platform will throw
+		// groovy.lang.MissingMethodException: No signature of method: ...
+		// here, but the door actually got the refresh command.
+		// If we don't catch the exception here we will stop after the first
+		// refresh, which is usually not enough.
+		// As a result, log and ignore.
+		log.debug "refresh failed with: $e"
+	}
 	if (timestamp >= data.stopAt) {
 		log.debug "stopping refreshing..."
 		unschedule(forceRefreshGarageState)
